9edbd4e49574ce769b16cb45fe6fba284bb84d63
George Papaya <78201554+georgepapaya@users.noreply.github.com>
2021-07-15 10:38:06 +0200

    Update portfolio.php

--- portfolio/portfolio.php
+++ portfolio/portfolio.php
@@ -3,6 +3,21 @@
     <title>Samson Banda - Portfolio</title>
     <link rel="stylesheet" href="css/style.css" type="text/css">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> 
+    <style> 
+            *{
+              box-sizing: border-box;
+            }
+            .header {
+              text-align: center;
+              padding: 32px;
+            }
+            .st1{
+                width:50%;
+            }
+            .st2{
+                width: 40%;
+            }
+    </style>
 </head>
 <body>
 <header id="top">
@@ -15,7 +30,27 @@
     </ul>
     </header>
     <div>
-        <h1>Portfolio</h1>
+        <div class="header"><h1>Portfolio Grid</h1></div>
+        <div class="row">
+            <div class="column">
+                <img src="images/cat.jpg" class="st1">
+                <img src="images/beach.jpg" class="st2">
+                <img src="images/cabin.jpg" class="st1">
+                <img src="images/cake.jpg" class="st2">
+            </div>
+            <div class="column">
+            <img src="images/camera.jpg" class="st2">
+            <img src="images/car.jpg" class="st1">
+            <img src="images/clouds.jpg" class="st2">
+            <img src="images/corridor.jpg" class="st1">
+            </div>
+            <div class="column">
+            <img src="images/design.jpg" class="st2">
+            <img src="images/lady.jpg" class="st1">
+            <img src="images/laptopdesk.jpg" class="st2">
+            <img src="images/mountains.jpg" class="st1">
+            </div>
+        </div>
     </div>
     <footer>Copyright 2021 &copy; Samson Banda</footer>
 </body>
